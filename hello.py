afa7bb801766014b92b78ce458cf206b80d17656
ParthBargale <parthbargale16@gmail.com>
2025-03-24 09:16:41 +0530

    Collabrator commit hello.py
    Parth,Prathamesh Dive and prathamesh Waghvade have forked in this repository created by Prathamesh Waghvade

--- hello.py
+++ hello.py
@@ -6,3 +6,5 @@ else:
 
 print("This will be added in the local envoirement")
 print("changes in  the local envoirement done")
+print("This is a Repository in collab with Prathamesh waghvade and Prathamesh Dive")
+print("We have changed some content in this py file and we gonna commit the changes")
